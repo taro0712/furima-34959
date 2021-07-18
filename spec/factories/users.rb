beb96846a70a9d34ba2d12a4d346ebb4a27e0312
taro0712 <asuka.t.1986@gmail.com>
2021-07-18 14:38:20 +0900

    ユーザ管理機能(2件指摘修正)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    email                 {'test@example'}
+    email                 {Faker::Internet.free_email}
     password              {'00a00b0'}
     password_confirmation {password}
     last_name             {'山田'}
